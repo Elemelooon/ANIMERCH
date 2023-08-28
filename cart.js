f0b2500de4d3a5c39503626550ae578af973a06c
elemelooon <rissaleijavier@gmail.com>
2023-08-28 21:11:00 +0800

    added more keychains

---
+++ cart.js
@@ -0,0 +1,39 @@
+function displayCart(){
+    let totalPrice = 0;
+    let getCart = JSON.parse(localStorage.getItem("myCart"));
+    let showItems = "";
+    let items = JSON.parse(localStorage.getItem("myCart"));
+    document.getElementById('itemcount').innerHTML = items.length;
+    getCart.forEach(
+        function(items, index){
+            let price = items.pPrice.toLocaleString('en-PH', {
+                style: 'currency',
+                currency: 'PHP'
+            });
+            showItems = showItems +
+            `<div class="cartitem" id="cartitem">
+            <img src=${items.pImg} id="itemimage">
+            <button onclick='remove(${index})' class="btn-close text-white" id="removebtn"></button>
+            <h3 id="itemname">${items.pName}</h3>
+            <p id="itemprice"> ${price}</p>
+            <hr>
+            </div>`;
+            totalPrice += items.pPrice;
+        }
+    );
+    let finalPrice = totalPrice.toLocaleString('en-PH', {
+        style: 'currency',
+        currency: 'PHP'
+        });
+    document.getElementById('cartlist').innerHTML = showItems;
+    document.getElementById('total').innerHTML = finalPrice;
+    document.getElementById('subtotal').innerHTML = finalPrice;
+}
+displayCart();
+
+function remove(index){
+    let getCart = JSON.parse(localStorage.getItem("myCart"));
+    let removedItem = getCart.splice(index, 1)[0];
+    localStorage.setItem("myCart", JSON.stringify(getCart));
+    displayCart(); 
+}
