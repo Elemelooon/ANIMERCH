ec2de40cbec87de301656a1a25381d4c699e2fd9
elemelooon <rissaleijavier@gmail.com>
2023-08-29 15:08:29 +0800

    sidecart

--- script.js
+++ script.js
@@ -339,43 +339,50 @@ function addCart(prodName, prodPrice, prodImg) {
 
     localStorage.setItem("myCart", JSON.stringify(cart));
     
+    sideCart();
 }
 
-function displayCart(){
+function sideCart(){
     let totalPrice = 0;
     let getCart = JSON.parse(localStorage.getItem("myCart"));
     let showItems = "";
+    // let items = JSON.parse(localStorage.getItem("myCart"));
+    // document.getElementById('itemcount').innerHTML = items.length;
     getCart.forEach(
-        function(items, index){
-            let price = items.pPrice.toLocaleString('en-PH', {
+        function(prods, index){
+            let price = prods.pPrice.toLocaleString('en-PH', {
                 style: 'currency',
                 currency: 'PHP'
             });
-            showItems = showItems +
-            `<div class="cartitem" id="cartitem">
-            <img src=${items.pImg} id="itemimage">
-            <button onclick='remove(${index})' class="btn-close text-white" id="removebtn"></button>
-            <h3 id="itemname">${items.pName}</h3>
-            <p id="itemprice"> ${price}</p>
-            <hr>
-            </div>`;
-            totalPrice += items.pPrice;
+            showItems = 
+            `<div class="row" id="cartitem">
+            <div class=" col-4">
+                <img src=${prods.pImg} id="sidecartimg">
+            </div>
+            <div class="cartdes col-8">
+                <h5 id="itemname">${prods.pName}</h5>
+                <p id="itemprice"> ${price}</p>
+                <hr>
+                </div>
+                <button onclick='removeside(${index})' class="btn-close text-white" id="removebtn"></button>
+            </div>` + showItems ;
+
+            totalPrice += prods.pPrice;
         }
     );
-    let finalPrice = totalPrice.toLocaleString('en-PH', {
+    let finalPrice = Number(totalPrice).toLocaleString('en-PH', {
         style: 'currency',
         currency: 'PHP'
         });
-    document.getElementById('cartlist').innerHTML = showItems;
-    document.getElementById('total').innerHTML = finalPrice;
+    document.getElementById('sidecart').innerHTML = showItems;
     document.getElementById('subtotal').innerHTML = finalPrice;
 }
-displayCart();
+sideCart();
 
-function remove(index){
+
+function removeside(index){
     let getCart = JSON.parse(localStorage.getItem("myCart"));
     let removedItem = getCart.splice(index, 1)[0];
     localStorage.setItem("myCart", JSON.stringify(getCart));
-    displayCart(); 
-}
-
+    sideCart(); 
+} 
